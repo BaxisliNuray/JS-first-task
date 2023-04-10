bff60133f2c5d6f3df269bc7afdc92a9970cbdc2
BaxisliNuray <bakhishlinuray@gmail.com>
2023-04-11 02:37:49 +0400

    adding js task

---
+++ script.js
@@ -0,0 +1,66 @@
+
+//  regular
+// function div(number) {
+//     if (number % 7 === 0) {
+//         console.log("divided to 7");
+//     }
+//     else {
+//         let a = parseInt(number / 7)
+//         const num1 = a * 7
+//         const num2 = (a + 1) * 7
+//         if (num2 - number > number - num1) {
+//             console.log(num1);
+//         }
+//         else {
+//             console.log(num2);
+//         }
+//     }
+
+// }
+// div(30);
+
+
+
+// anonym
+// let divide=function (numb){
+//     if (numb % 7 === 0) {
+//         console.log("divided to 7");
+//     }
+//     else {
+//         let a = parseInt(numb / 7)
+//         const numb1 = a * 7
+//         const numb2 = (a + 1) * 7
+//         if (numb2 - numb > numb - numb1) {
+//             console.log(numb1);
+//         }
+//         else {
+//             console.log(numb2);
+//         }
+//     }
+   
+// }
+// divide(70);
+
+
+
+// //  arrow
+// let division= (numb)=>{    
+//     if (numb % 7 === 0) {
+//         console.log("divided to 7");
+//     }
+//     else {
+//         let a = parseInt(numb / 7)
+//         const numb1 = a * 7
+//         const numb2 = (a + 1) * 7
+//         if (numb2 - numb > numb - numb1) {
+//             console.log(numb1);
+//         }
+//         else {
+//             console.log(numb2);
+//         }
+//     }
+   
+// }
+// division(70);
+
+
---
+++ script2.js
@@ -0,0 +1,87 @@
+// // regular
+// function divide(num) {
+//     var conunt = 0;
+//     if (num < 50) {
+//         for (let index = 1; index < num; index++) {
+//             if (index % 3 === 0) {
+//                 conunt++;
+//             }
+//         }
+//     }
+//     else if (num > 50 && num < 100) {
+//         for (let index = 50; index < num; index++) {
+//             if (index % 5 === 0) {
+//                 conunt++;
+//             }
+//         }
+//     }
+//     else if(num>100){
+//         for (let index = 0; index < num; index++) {
+//           if(index%8===0){
+//             conunt++;
+//           }
+//         }
+//     }
+//     console.log(conunt);
+// }
+// divide(40)
+
+
+
+// // anonym
+// let divide=function(num){
+//     var conunt = 0;
+//     if (num < 50) {
+//         for (let index = 1; index < num; index++) {
+//             if (index % 3 === 0) {
+//                 conunt++;
+//             }
+//         }
+//     }
+//     else if (num > 50 && num < 100) {
+//         for (let index = 50; index < num; index++) {
+//             if (index % 5 === 0) {
+//                 conunt++;
+//             }
+//         }
+//     }
+//     else if(num>100){
+//         for (let index = 0; index < num; index++) {
+//           if(index%8===0){
+//             conunt++;
+//           }
+//         }
+//     }
+//     console.log(conunt);
+// }
+// divide(80)
+
+
+// // arrow
+// let divide=(num)=>{
+//     var conunt = 0;
+//     if (num < 50) {
+//         for (let index = 1; index < num; index++) {
+//             if (index % 3 === 0) {
+//                 conunt++;
+//             }
+//         }
+//     }
+//     else if (num > 50 && num < 100) {
+//         for (let index = 50; index < num; index++) {
+//             if (index % 5 === 0) {
+//                 conunt++;
+//             }
+//         }
+//     }
+//     else if(num>100){
+//         for (let index = 0; index < num; index++) {
+//           if(index%8===0){
+//             conunt++;
+//           }
+//         }
+//     }
+//     console.log(conunt);
+// }
+// divide(200)
+
